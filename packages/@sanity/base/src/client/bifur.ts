83f75dd1ff02b7c625c4cc6363e20a13dbde2bb9
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-06-30 16:00:53 +0200

    feat(base): support presence for cookieless auth users

--- packages/@sanity/base/src/client/bifur.ts
+++ packages/@sanity/base/src/client/bifur.ts
@@ -4,5 +4,10 @@ import {versionedClient} from './versionedClient'
 
 export const bifur = fromSanityClient(
   // The global Sanity client is guaranteed to have a dataset, thus the type cast
-  versionedClient as SanityClient & {config(): ClientConfig & {dataset: string}}
+  versionedClient.withConfig({
+    apiVersion: '2022-06-30',
+    ignoreBrowserTokenWarning: true,
+  }) as SanityClient & {
+    config(): ClientConfig & {dataset: string}
+  }
 )
